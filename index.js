43a53c5d2b3b5d294e403c015c9d3af84f423e1c
Bilal <bilalmohammed261@gmail.com>
2023-07-20 16:36:25 -0400

    second comit for assignment 5 & 6

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//Submitted by Bilal Mohammed(C0882358)
 class Car{
     
     constructor(brand, model, year, color, price,gas)
@@ -12,40 +13,50 @@ class Car{
     }
     honk()
     {
-        console.log("Tuut tuut");
+        console.log(`${this.brand} car Tuut tuut`);
         console.log(`Brand: ${this.brand}  Model: ${this.model}  Year:${this.year}  Color:${this.color} Price:${this.price}$ Gas: ${this.gas}`);
     }
 
-    race()
+    race(i,today)
     {
-        let today = new Date();
+        
         this.gas -= 5 + (today.getFullYear()-this.year);
-        console.log("After race");
-        console.log(this);
+        console.log(`${this.brand} car has ${this.gas} gas value`);
     }
 
     
 }
 
+let objectsArray = [];
 let car1 = new Car("Honda","CR-V",2023,"Red",50000,45);
+objectsArray.push(car1);
 let car2 = new Car("Ford","F-150",2020,"Black",25000,30);
+objectsArray.push(car2);
 let car3 = new Car("BMW","X5",2022,"Green",60000,65);
+objectsArray.push(car3);
 let car4 = new Car("Mazda","CX-5",2019,"White",15000,60);
+objectsArray.push(car4);
 let car5 = new Car("Audi","Q7",2018,"Silver",52000,47);
+objectsArray.push(car5);
 let car6 = new Car("Kia","Forte",2020,"Blue",21000,56);
+objectsArray.push(car6);
 
-car1.honk();
-car2.honk();
-car3.honk();
-car4.honk();
-car5.honk();
-car6.honk();
 
-//for(let i=0;i<7;i++){
-    car1.race();
-    car2.race();
-    car3.race();
-    car4.race();
-    car5.race();
-    car6.race();
-//}
+for(let obj of objectsArray)
+{
+    obj.honk();
+}
+
+
+
+ let today = new Date();
+ let turns = 7;
+ for(let i=0;i<turns;i++)
+ {
+    console.log(`After turn ${i+1}`);
+    for(let j=0;j<objectsArray.length;j++)
+    {
+       objectsArray[j].race(i,today);
+    }
+ }
+
